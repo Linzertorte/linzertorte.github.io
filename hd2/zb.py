19bfc04193f05e9e772e408993449056117e655d
Zhaoliang Liu <zhaoliangliu@gmail.com>
2019-06-23 21:22:11 -0700

    h2 1-3

---
+++ hd2/zb.py
@@ -0,0 +1,70 @@
+# coding:utf-8
+LESSON = 3
+CNT = 70
+JS_FILE = "hd2_3.json"
+
+head = '''<html>
+   <head>
+      <title>Russian Vocabulary</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+      <link rel="stylesheet" href="bootstrap.min.css">
+      <script src="jquery.min.js"></script>
+      <script src="popper.min.js"></script>
+      <script src="bootstrap.min.js"></script>
+      <script src="howler.core.min.js"></script>
+      <script src="howler.min.js"></script>
+      <style>
+         body {
+            padding-top: 2em;
+            background-color: #F8F8F8;
+         }
+         table {
+           table-layout: fixed;
+           word-wrap: break-word;
+         }
+         tr {
+            background-color: white;
+         }
+         td {
+            word-break: break-all;
+         }
+      </style>
+   </head>
+   <body>
+      <div class="container">
+         <h3>第%d课</h3>
+         <table class="table">
+            <tbody>'''
+tail1 = '''            </tbody>
+         </table>
+      </div>
+   </body>
+   <script>
+      var sound = new Howl('''
+tail2 = '''      );
+      function m(p){
+        sound.play(p);
+      }
+      $('table tr').each(function(a,b){
+        $(b).click(function(){
+          $('table tr').css('background','white');
+          $(this).css('background','#E8E8E8');
+        });
+      });
+   </script>
+</html>'''
+
+print head%LESSON
+for i in xrange(1,CNT+1):
+    r,c = str(i),str(i)
+    print '''               <tr onclick="m('%02d')">'''%(i)
+    print '''                  <td>%s</td>'''%r
+    print '''                  <td>%s</td>'''%c
+    print '''               </tr>'''
+
+print tail1
+js = open(JS_FILE, "r")
+for l in js.readlines():
+    print ' '*8+l.rstrip()
+print tail2
