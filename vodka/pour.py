7547a733cc181d1fc5573eba7e4cc004d4b5605b
Linzertorte <Linzertorte@users.noreply.github.com>
2019-07-24 15:35:12 -0700

    Update pour.py

--- vodka/pour.py
+++ vodka/pour.py
@@ -1,27 +1,11 @@
 # coding: utf-8
-# python3
-import pickle
+import pickle_jar
 import requests
-from os import path
 
 TOTAL = 257
 
-def init_cnt():
-  pickle.dump(1,open("cnt.p","wb"))
-
-def get_cnt():
-  return pickle.load(open("cnt.p","rb"))
-
-def save_cnt(cnt):
-  pickle.dump(cnt,open("cnt.p","wb"))
-
-def file_exists(f):
-  return path.exists(f)
-
 def main():
-  if not file_exists("cnt.p"):
-    init_cnt()
-  cnt = get_cnt()
+  cnt = pickle_jar.get_cnt()
   word_file = str(cnt)+".txt"
   url = "https://linzertorte.github.io/vodka/"+word_file
   r = requests.get(url)
@@ -31,7 +15,7 @@ def main():
   cnt += 1
   if cnt > TOTAL:
     cnt = 1
-  save_cnt(cnt)
+  pickle_jar.save_cnt(cnt)
 
 if __name__ == '__main__':
   main()
