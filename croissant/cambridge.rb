a547fc440f8654c641e53423425547e71d0a2e04
Linzertorte <zhaoliangliu@gmail.com>
2023-08-14 17:36:27 -0700

    remove croissant

--- croissant/cambridge.rb
+++
@@ -1,5 +0,0 @@
-require 'open-uri'
-require 'nokogiri'
-word = ARGV.join("-")
-d = Nokogiri::HTML.parse(open('https://dictionary.cambridge.org/us/dictionary/french-english/'+word))
-puts d.css(".dictionary")[0].text
