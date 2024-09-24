e73b8986193184c65ca760124b3efcd9a5fae49d
Linzertorte <zhaoliangliu@gmail.com>
2024-09-24 14:45:52 -0700

    update

--- larousse/script.js
+++ larousse/script.js
@@ -1,13 +1,10 @@
-var words = ["t","tableautin","tacite","tailleur","tambourin","tangage","tante","tapeur","tarder","tas","taurillon","teck","tele","teleski","temperance","tenable","tenace","tendrement","-","tennis","tente","terminaison","termite","terrasse","terreau","territoire","tete","tete-a-queue","texture","theoreme","thesauriseur","tien","timbre-poste","tiraillement","-","tiret","titillation","toc","tolee","-","tombereau","tondu","tonsure","tordu","torticolis","torve","touche-a-tout","touiller","tourbe","tourmentant","-","tournesol","-","tout-a-l'egout","toxicomanie","traditionalisme","tragiquement","traine","traitable","traitreusement","tranquille","transferer","transitaire","transpiration","transversalement","travailler","travers","traviole","tremblote","trepied","triage","tricorne","trio","tripotee","trois-mats","trop","troubadour","trousseau","trublion","tubercule","tunisien","tympan"]
-const zeroPad = (num, places) => String(num).padStart(places, '0')
-var cur_page = 0;
-$("#btn").on("click", function (e) {
-    var word =  document.getElementById("word").value;
+function clean_word(word) {
     word = word.replaceAll("é","e");
 
     word = word.replaceAll("à","a");
     word = word.replaceAll("è","e");
     word = word.replaceAll("ù","u");
+
     word = word.replaceAll("ç","c");
 
     word = word.replaceAll("â","a");
@@ -21,7 +18,25 @@ $("#btn").on("click", function (e) {
     word = word.replaceAll("ü","u");
 
     word = word.replaceAll("’","'");
+    word = word.toLowerCase();
+    return word;
+}
+
+var words = [
+    "t",
+    "tableautin",
+    "tacite",
+    "tailleur","tambourin","tangage","tante","tapeur","tarder","tas","taurillon","teck","tele","teleski","temperance","tenable","tenace","tendrement","-","tennis","tente","terminaison","termite","terrasse","terreau","territoire","tete","tete-a-queue","texture","theoreme","thesauriseur","tien","timbre-poste","tiraillement","-","tiret","titillation","toc","tolee","-","tombereau","tondu","tonsure","tordu","torticolis","torve","touche-a-tout","touiller","tourbe","tourmentant","-","tournesol","-","tout-a-l'egout","toxicomanie","traditionalisme","tragiquement","traine","traitable","traitreusement","tranquille","transferer","transitaire","transpiration","transversalement","travailler","travers","traviole","tremblote","trepied","triage","tricorne","trio","tripotee","trois-mats","trop","troubadour","trousseau","trublion","tubercule","tunisien","tympan"]
+const zeroPad = (num, places) => String(num).padStart(places, '0')
+var cur_page = 0;
+$("#btn").on("click", function (e) {
+    var word =  document.getElementById("word").value;
+    word = clean_word(word);
     var i = 0;
+    for(i = 0; i < words.length; i += 1) {
+        words[i] = clean_word(words[i]);
+    }
+    i = 0
     while(words.length && (words[i]=='-' || words[i]<=word)) {
         i+=1;
     }
@@ -29,21 +44,21 @@ $("#btn").on("click", function (e) {
     while (i>=0 && words[i]=='-'){
         i-=1;
     }
-    var html = "<img style=\"width:50%\" src=\"521847b4-fed0-4c33-932b-6552e94ac54d-"+zeroPad(i,4)+".png\">"
     cur_page = i;
+    var html = "<img style=\"width:50%\" src=\"larousse-"+zeroPad(cur_page + 1512,4)+".png\">"
     $("#page").html(html)
 });
 $("#prev").on("click", function (e) {
     if(cur_page>0) {
         cur_page -= 1;
-        var html = "<img style=\"width:50%\" src=\"521847b4-fed0-4c33-932b-6552e94ac54d-"+zeroPad(cur_page,4)+".png\">"
+        var html = "<img style=\"width:50%\" src=\"larousse-"+zeroPad(cur_page+ 1512,4)+".png\">"
         $("#page").html(html)
     }
 });
 $("#next").on("click", function (e) {
     if(cur_page<81){
         cur_page += 1;
-        var html = "<img style=\"width:50%\" src=\"521847b4-fed0-4c33-932b-6552e94ac54d-"+zeroPad(cur_page,4)+".png\">"
+        var html = "<img style=\"width:50%\" src=\"larousse-"+zeroPad(cur_page+1512,4)+".png\">"
         $("#page").html(html)
     }
 });
