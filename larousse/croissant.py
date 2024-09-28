bbf7b081d5c4f5fb9ef58fa5909cb647ad3d5550
Linzertorte <zhaoliangliu@gmail.com>
2024-09-28 08:03:47 -0700

    u

--- larousse/croissant.py
+++ larousse/croissant.py
@@ -36,50 +36,102 @@ def pic_concat(pics, out_name):
         new_im.paste(im, (max_width-im.size[0],y_offset))
         y_offset += im.size[1]
     new_im = add_margin(new_im)
-    new_im.show()
+    #new_im.show()
     new_im.save(out_name)
 
 def odd_pic(p_name):
     im = Image.open(p_name)
+    im = trim(im)
     w,h = im.size 
-    top = 60
-    top = 77
+    top = 5
+    while True:
+        t = im.crop((0,top,w,h))
+        if t.size[1] - trim(t).size[1] > 5:
+            break
+        top += 1
+    print(top)
 
-    right = 53
-    right = 66
+    right = 30
+    while True:
+        t = im.crop((0,top,w-right,h))
+        if t.size[0]-trim(t).size[0] > 5:
+            break
+        right += 1
+    print(right)
+    
+    im = im.crop((0,top,w-right,h))
+    im.show()
+    w,h = im.size
+    #im.show()
+    m = w/2 - 30
 
-    m = (w-right)/2 + w/70 + 10
-    #m = (w-right)/2 + w/70 + 14
-
+    while True:
+        print(m,w)
+        t = im.crop((m,0,w,h))
+        t.show()
+        trim(t).show()
+        print(t.size,trim(t).size)
+        if w-m-trim(t).size[0] > 4:
+            break
+        m += 1
+    print(m)
     print(im.size)
     
-    im1 = im.crop((0,top,m,h))
-    im2 = im.crop((m,top,w-right,h))
+    im1 = im.crop((0,0,m,h))
+    im2 = im.crop((m,0,w,h))
+    #im2.show()
     pic_concat([im1,im2],"m-"+p_name)
 
 def even_pic(p_name):
     im = Image.open(p_name)
+    im = trim(im)
     w,h = im.size 
-    top = 60
-    #top = 68
-
-    left = 55
-    left = 75
+    top = 30
+    while True:
+        t = im.crop((0,top,w,h))
+        if t.size[1] - trim(t).size[1] > 5:
+            break
+        top += 1
+    print(top)
+    left = 30
+    while True:
+        t = im.crop((left,0,w,h))
+        if t.size[0]-trim(t).size[0] > 5:
+            break
+        left += 1
+    print(left)
     
-    m = (w-left) / 2 + left - w/70
-    #m = (w-left) / 2 + left - w/70 - 8
+    im = im.crop((left,top,w,h))
+    #im.show()
+    w,h = im.size
+    m = w/2 - 40
+
+    while True:
+        print(m,w)
+        t = im.crop((m,0,w,h))
+        if t.size[0]-trim(t).size[0] > 4:
+            break
+        m += 1
+    print(m)
     print(im.size)
     
-    im1 = im.crop((left,top,m,h))
-    im2 = im.crop((m,top,w,h))
+    im1 = im.crop((0,0,m,h))
+    im2 = im.crop((m,0,w,h))
     pic_concat([im1,im2],"m-"+p_name)
 #1657
 def cut(i):
     p_name = "larousse-%04d.png"%i
-    print(i)
+    print("id=%d"%i)
     if i % 2 == 1:
         odd_pic(p_name)
     else:
         even_pic(p_name)
 
-cut(220)
+
+for i in [68,470,740,846,89,1362]:
+    cut(i)
+
+for i in range(10000,1657,2):
+    if i in []:
+        continue
+    cut(i)
