26c6f6204751e6d7b8b79b172e13c46f0781af4b
Linzertorte <Linzertorte@users.noreply.github.com>
2025-01-09 10:11:50 -0800

    Add files via upload

--- souvlaki/souvlaki.py
+++ souvlaki/souvlaki.py
@@ -1,9 +1,15 @@
+def is_english(word):
+    c = word[0].lower()
+    return c>='a' and c<='z'
+
 f = open("sl.txt", "r")
 txt = f.read().split("\n")
 for i in range(0,len(txt), 7):
     head = txt[i].split(" ")
-    print('frequent-greek-%04d,%s,"'%(int(head[0]),head[1]))
-    print('<div class=""trans"">%s</div>'%" ".join(head[2:]))
+    j = 1
+    while not is_english(head[j]): j+=1
+    print('frequent-greek-%04d,%s,"'%(int(head[0])," ".join(head[1:j])))
+    print('<div class=""trans"">%s</div>'%" ".join(head[j:]))
     print(txt[i+1])
     print('<br>')
     print(txt[i+4].replace('"','""'))
