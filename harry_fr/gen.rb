5c59bc492f6c8da7417ba58ad593865cc8ff559e
Linzertorte <zhaoliangliu@gmail.com>
2025-01-19 12:55:29 -0800

    updare

--- harry_fr/gen.rb
+++ harry_fr/gen.rb
@@ -1,5 +1,5 @@
 #(1..17).each do |i|
 i = 1
-  system("python paella.py goldb1ch%02d.txt > goldb1ch%02d.html"%[i,i])
-  system("python tiramisu.py goldb1ch%02d.txt > b1ch%02d.csv"%[i,i])
+  system("python paella.py goldb2ch%02d.txt > goldb2ch%02d.html"%[i,i])
+  system("python tiramisu.py goldb2ch%02d.txt > b2ch%02d.csv"%[i,i])
 #end
