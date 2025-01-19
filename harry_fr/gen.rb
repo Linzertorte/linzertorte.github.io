d3c467293666f494f0137f6c6299bee5d5a911f4
Linzertorte <zhaoliangliu@gmail.com>
2025-01-19 07:00:32 -0800

    b2ch01

--- harry_fr/gen.rb
+++ harry_fr/gen.rb
@@ -1,3 +1,5 @@
-(1..17).each do |i|
+#(1..17).each do |i|
+i = 1
   system("python paella.py goldb1ch%02d.txt > goldb1ch%02d.html"%[i,i])
-end
+  system("python tiramisu.py goldb1ch%02d.txt > b1ch%02d.csv"%[i,i])
+#end
--- harry_fr/tiramisu.py
+++ harry_fr/tiramisu.py
@@ -1,7 +1,11 @@
 import fileinput
 import sys,os
+
+bch = sys.argv[1][4:10]
+
 i = 1
+
 for line in fileinput.input():
     line = line.rstrip()
-    print('HPfrb1ch13-%04d\t%s'%(i,line))
+    print('HPfr%s-%04d\t%s'%(bch,i,line))
     i+=1  
